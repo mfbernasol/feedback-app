080593cee0c0969d31488b2a0baba0a52fcfcf84
mfbernasol <mfbernasol@gmail.com>
2023-02-22 23:50:01 -0600

    create FeedbackStats component and delete functionality

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
+import FeedbackStats from './components/FeedbackStats';
 
 function App() {
+  const [feedback, setFeedBack] = useState(FeedbackData);
 
-  const [feedback, setFeedBack] = useState(FeedbackData)
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedBack(feedback.filter((item) => item.id !== id));
+    }
+  };
 
   return (
     <>
       <Header />
       <div className='container'>
-        <FeedbackList feedback={feedback} />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,28 +1,32 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import React from 'react';
 import FeedbackItem from './FeedbackItem';
 
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback yet</p>;
   }
   return (
     <div className='feedback-list'>
-        {feedback.map((item)=> (
-            <FeedbackItem key={item.id} item={item}/>
-        ))}
+      {feedback.map((item) => (
+        <FeedbackItem
+          key={item.id}
+          item={item}
+          handleDelete={handleDelete}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
 FeedbackList.propTypes = {
-    feedback: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            text: PropTypes.string.isRequired,
-            rating: PropTypes.number.isRequired
-        })
-    )
-}
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
 
 export default FeedbackList;

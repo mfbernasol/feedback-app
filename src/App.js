a1ab72250ade2910c578b2bc3f9091d6f097a218
mfbernasol <mfbernasol@gmail.com>
2023-02-23 22:09:21 -0600

    create Feedback form component with validation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
 
 function App() {
   const [feedback, setFeedBack] = useState(FeedbackData);
+
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedBack([newFeedback,...feedback])
+  };
 
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
@@ -17,6 +25,7 @@ function App() {
     <>
       <Header />
       <div className='container'>
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>

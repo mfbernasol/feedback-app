6b88709258e34a22221ae970a3f166a2a145a093
mfbernasol <mfbernasol@gmail.com>
2023-02-24 22:52:38 -0600

    fix react router for v6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
@@ -26,13 +26,24 @@ function App() {
     <Router>
       <Header />
       <div className='container'>
-        <Route exact path='/'>
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-        </Route>
+        <Routes>
+          <Route
+            exact
+            path='/'
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
 
-        <Route path='/about' component={AboutPage} />
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
       </div>
     </Router>
   );

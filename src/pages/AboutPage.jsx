87cec5866f383646cfde6e81258550c438d6b8de
mfbernasol <mfbernasol@gmail.com>
2023-02-25 20:53:15 -0600

    create AboutPage and setup routing to pages

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from '../components/shared/Card';
+import {Link} from 'react-router-dom'
 function AboutPage() {
   return (
     <Card>
@@ -7,6 +8,9 @@ function AboutPage() {
         <h1>About this Project</h1>
         <p>This is a React app to leave feedback for a product or service</p>
         <p>Version: 1.0.0</p>
+        
+        <p><Link to='/'> Back to Home</Link></p>
+
       </div>
     </Card>
   );
